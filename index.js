a523168cc7d0be3a5657a3d76da1739cc1411b1d
himaloyash <himaloyashis98@gmail.com>
2024-05-06 22:33:43 +0600

    the port is on

---
+++ index.js
@@ -0,0 +1,27 @@
+//
+const http = require('http');
+
+// app object - module scaffolding
+
+const app = {};
+
+//Configuration
+app.config= {
+    port: 3000
+};
+
+//createServer
+app.createServer = ()=>{
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.port, ()=>{
+        console.log(`listening the port ${app.config.port}`);
+    })
+};
+
+//handle request response
+app.handleReqRes = (req, res)=>{
+    //response handle
+    res.end('Hello World');
+}
+
+
