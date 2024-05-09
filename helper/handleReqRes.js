f4e35f93a7a81117086ecd1f75c47a1602e070df
himaloyash <himaloyashis98@gmail.com>
2024-05-09 11:11:40 +0600

    handle req res is done

--- helper/handleReqRes.js
+++ helper/handleReqRes.js
@@ -1,3 +1,48 @@
 /**
  * 
+ * Title: Req and res handler
+ * Description: It is a helper for handle request.
+ * 
+ * 
+ * 
 */
+
+// Dependencies
+
+const url = require('url');
+const { StringDecoder } = require('string_decoder')
+
+
+const handler = {};
+
+app.handleReqRes = (req, res)=>{
+    //response handle
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmEdPath = path.replace(/^\/+|\/+$/g, '');
+    const method = req.method.toLowerCase()
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+
+    const decoder = new StringDecoder('utf-8'); 
+    let realData = '';
+
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer)
+
+    })
+
+    req.on('end', () =>{
+        realData += decoder.end();
+        console.log(realData);
+
+        res.end('Hello World'); 
+
+    })
+
+}
+
+
+module.exports = handler;
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 //
 const http = require('http');
 
-const url = require('url');
-const { StringDecoder } = require('string_decoder')
+const {handleReqRes }  = require('./helper/handleReqRes');
 
 // app object - module scaffolding
 const app = {};
@@ -20,38 +19,9 @@ app.createServer = ()=>{
     })
 };
 
+app.handleReqRes = handleReqRes;
 
 //handle request response
-app.handleReqRes = (req, res)=>{
-    //response handle
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmEdPath = path.replace(/^\/+|\/+$/g, '');
-    const method = req.method.toLowerCase()
-    const queryStringObject = parsedUrl.query;
-    const headersObject = req.headers;
-
-
-    const decoder = new StringDecoder('utf-8'); 
-    let realData = '';
-
-
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer)
-
-    })
-
-    req.on('end', () =>{
-        realData += decoder.end();
-        console.log(realData);
-
-        res.end('Hello World'); 
-
-    })
-
-
-
-}
 
 app.createServer();
 
