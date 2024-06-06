3a3e6bf42d4bbe82f84ff634b1c71cd4201ebea0
himaloyash <himaloyashis98@gmail.com>
2024-06-06 23:50:35 +0600

    ok i will start soon

--- handler/routeHandler/sampleHandler.js
+++ handler/routeHandler/sampleHandler.js
@@ -13,7 +13,7 @@ handler.sampleHandler = ( requestProperties, callback) =>{
     console.log(requestProperties);
     callback(200, { 
         message : "This is a sample url"
-    })
+    }) 
 }
 
 
